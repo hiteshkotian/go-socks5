2bbdcb11f79604c3ee9bf821c2d87e1f58eda36f
Hitesh Chidambar Kotian <hckotian@us.ibm.com>
2020-05-09 00:26:37 -0400

    Adding outbound handler and cleaning up code

--- cmd/proxy_main.go
+++ cmd/proxy_main.go
@@ -1,3 +1,4 @@
+// Package main contains the main entry point for the proxy
 package main
 
 import (
@@ -9,13 +10,26 @@ import (
 	"syscall"
 )
 
+// Main entry point of the proxy
 func main() {
-	proxy := proxy.New("server1", 8080)
+	// Set the proxy properties
+	// TODO either accept flags or configuration file
+	// to bootstrap the proxy
+	name := "server1"
+	port := 8080
+	maxConnCount := 2
+	// Create an instance of the proxy
+	proxy := proxy.New(name, port, maxConnCount)
+
+	// Setup the close handlers to handle interrupts
 	setupCloseHandler(proxy)
 
+	// Start the proxy
 	proxy.Start()
 }
 
+// setupCloseHandler function registers SIGTERM signal
+// to gracefully shutdown the server
 func setupCloseHandler(proxy *proxy.Server) {
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
--- handler/manager.go
+++ handler/manager.go
@@ -1,6 +1,22 @@
 package handler
 
-import "net"
+import (
+	"net"
+	"time"
+)
+
+type Request struct {
+	id   int64
+	conn net.Conn
+	data []byte
+	host string
+}
+
+func NewRequest(conn net.Conn, data []byte) *Request {
+	id := time.Now()
+	request := Request{id: id.Unix(), conn: conn, host: "www.google.com:443", data: data}
+	return &request
+}
 
 // Handler interface defines the basic function
 // to be implemented by a handler
@@ -12,7 +28,9 @@ type Handler interface {
 	// NewHandlerWithConfig(map[string]string) (Handler, error)
 	// HandleRequest will handle the incoming request
 	// and subsequently handle the response
-	HandleRequest(net.Conn) error
+	// HandleRequest(net.Conn) error
+
+	HandleRequest(*Request) error
 }
 
 // HandlerContext object is passed around between
@@ -27,8 +45,8 @@ type Proxy struct {
 	HandlerChain []Handler
 }
 
-func (p *Proxy) ExecuteHandlers() {
+func (p *Proxy) ExecuteHandlers(request *Request) {
 	for _, handler := range p.HandlerChain {
-		handler.HandleRequest(nil)
+		handler.HandleRequest(request)
 	}
 }
